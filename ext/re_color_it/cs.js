4542f5c41fcff1af0550e2d4824c2d9b64bc48be
mission.liao <mac@missionliao.local>
2011-12-06 23:57:24 +0800

    daily dev

--- ext/re_color_it/cs.js
+++ ext/re_color_it/cs.js
@@ -2,9 +2,81 @@
 
 // TODO: read current color theme from background-page
 
-// iterate through DOM tree, and convert their color to gray
-var elms = document.documentElement.getElementByTagName("*");
-for (var i in elms) {
-    console.log(elms[i].tagName);
+var styleProp2check = [
+    // background
+    "backgroundColor",
+    // border/outline
+    "borderBottomColor",
+    "borderLeftColor",
+    "borderRightColor",
+    "borderTopColor",
+    "outlineColor",
+    // text
+    "color",
+];
+
+var clrIO = [
+    {
+        re: new RegExp("rgb\((\d+),\s*(\d+),\s*(\d+)\)", "i"),
+        inf: function(m) {
+            c = [];
+            c.push(parseInt(m[1]);
+            c.push(parseInt(m[2]);
+            c.push(parseInt(m[3]);
+            return c;
+        }
+        outf: function (c) {
+            return "rgb(" + c[1] + "," + c[2] + "," + c[3] + ")";
+        }
+    },
+    {
+        re: new RegExp("rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)", "i"),
+        inf: function(m) {
+            c = [];
+            c.push(parseInt(m[1]);
+            c.push(parseInt(m[2]);
+            c.push(parseInt(m[3]);
+            c.push(parseFloat(m[4]);
+            return c;
+        }
+        outf: function (c) {
+            return "rgba(" + c[1] + "," + c[2] + "," + c[3] + "," + c[4] + ")";
+        }
+    }
+];
+
+
+var qElm = [document.body];
+while (qElm.length) {
+    // pop the front one as current element
+    curElm = qElm.shift();
+
+    // check if there is style information in current element
+    s = window.getComputedStyle(curElm, null);
+    if (s) {
+        for (ip in styleProp2check) {
+            if (styleProp2check[ip] in s) {
+                // revert it color
+                ss = s[styleProp2check[ip]];
+
+                for (ci in clrIO) {
+                    m = ss.match(clrIO[ci].re);
+                    if (m) {
+                        c = clrIO.inf(m);
+                        c[0] = 255 - c[0];
+                        c[1] = 255 - c[1];
+                        c[2] = 255 - c[2];
+                        curElm.style[styleProp2check[ip] = clrIO.outf(c);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    for (i in curElm.childNodes) {
+        qElm.push(curElm.childNodes[i]);
+    }
 }
+
 })();
