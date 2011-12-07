d5f70befa621aa8049b4c227deeaecef19a95acc
mission.liao <mac@missionliao.local>
2011-12-07 08:09:46 +0800

    daily dev

--- ext/re_color_it/cs.js
+++ ext/re_color_it/cs.js
@@ -17,39 +17,44 @@ var styleProp2check = [
 
 var clrIO = [
     {
-        re: new RegExp("rgb\((\d+),\s*(\d+),\s*(\d+)\)", "i"),
+        t: "rgb",
+        re: /rgb\((\d+),\s*(\d+),\s*(\d+)\)/i,
         inf: function(m) {
             c = [];
-            c.push(parseInt(m[1]);
-            c.push(parseInt(m[2]);
-            c.push(parseInt(m[3]);
+            c.push(parseInt(m[1]));
+            c.push(parseInt(m[2]));
+            c.push(parseInt(m[3]));
             return c;
-        }
+        },
         outf: function (c) {
-            return "rgb(" + c[1] + "," + c[2] + "," + c[3] + ")";
+            return "rgb(" + c[0] + "," + c[1] + "," + c[2] + ")";
         }
     },
     {
-        re: new RegExp("rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)", "i"),
+        t: "rgba",
+        re: /rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)/i,
         inf: function(m) {
             c = [];
-            c.push(parseInt(m[1]);
-            c.push(parseInt(m[2]);
-            c.push(parseInt(m[3]);
-            c.push(parseFloat(m[4]);
+            c.push(parseInt(m[1]));
+            c.push(parseInt(m[2]));
+            c.push(parseInt(m[3]));
+            c.push(parseFloat(m[4]));
             return c;
-        }
+        },
         outf: function (c) {
-            return "rgba(" + c[1] + "," + c[2] + "," + c[3] + "," + c[4] + ")";
+            return "rgba(" + c[0] + "," + c[1] + "," + c[2] + "," + c[3] + ")";
         }
     }
 ];
 
+// TODO: we can iterate through ul, li tags in these code
 
 var qElm = [document.body];
 while (qElm.length) {
     // pop the front one as current element
     curElm = qElm.shift();
+    if (curElm.tagName == "li")
+        console.log(curElm.innerText);
 
     // check if there is style information in current element
     s = window.getComputedStyle(curElm, null);
@@ -59,16 +64,27 @@ while (qElm.length) {
                 // revert it color
                 ss = s[styleProp2check[ip]];
 
+                bMatch = false;
                 for (ci in clrIO) {
                     m = ss.match(clrIO[ci].re);
                     if (m) {
-                        c = clrIO.inf(m);
+                        c = clrIO[ci].inf(m);
                         c[0] = 255 - c[0];
                         c[1] = 255 - c[1];
                         c[2] = 255 - c[2];
-                        curElm.style[styleProp2check[ip] = clrIO.outf(c);
+                        curElm.style[styleProp2check[ip]] = clrIO[ci].outf(c);
+
+                        if (styleProp2check[ip] == "color" && curElm.innerText == "The first summit of ") {
+                            console.log(clrIO[ci].outf(c));
+                        }
+
+                        bMatch = true;
                         break;
                     }
+                }
+
+                if (!bMatch) {
+                    console.log("not match: " + ss);
                 }
             }
         }
