2d2d56438237a40c184eef205373badbc1ebc938
missionliao <missionliao@missionliao-OptiPlex-760.(none)>
2011-12-09 18:05:24 +0800

    daily dev

--- ext/re_color_it/cs.js
+++ ext/re_color_it/cs.js
@@ -41,8 +41,48 @@ var clrIO = [
     }
 ];
 
-// TODO: we can iterate through ul, li tags in these code
+function gen_unique_id_for_element(elm, prefix) {
+    if (id in elm && elm.id != "")
+        return elm.id;
+    }
 
+    if (typeof prefix == "string") {
+        id = prefix;
+    }
+
+    // generate our id with some meaningful part
+    if (prefix == undefined || prefix == null || prefix == "") {
+        // traverse back to parent node
+        qElm = [];
+        curElm = elm;
+        while (curElm != null && curElm.nodeType == 1) {
+            qElm.push(curElm);
+            curElm = curElm.parentNode;
+        }
+        qElm.reverse();
+
+        // concate id/tag-name as part of the new id
+        for(i in qElm) {
+            curS = "";
+            if (qElm[i].id != undefined && qElm[i].id != "") {
+                curS = qElm[i].id;
+            } else {
+                curS = qElm[i].tagName;
+            }
+
+            if (curS != "") {
+                if (id.length != 0) id += ".";
+                id += curS;
+            }
+        }
+    }
+
+    //  handle for multiple elements with the same tagName and same parent-node
+    randV = Math.random() * 0xFFFFFF;
+    return id += randV;
+}
+
+var cacheStyle = {};
 var qElm = [document.body];
 while (qElm.length) {
     // pop the front one as current element
@@ -60,34 +100,44 @@ while (qElm.length) {
                     continue;
                 }
 
-                bMatch = false;
+                sChanged = {};
                 for (ci in clrIO) {
                     m = ss.match(clrIO[ci].re);
                     if (m) {
+                        // before we modify it, we need to cache it original value
+                        // for caching, we need an id and assign one if this element doesn't have one.
+                        sChanged[styleProp2check[ip]] = ss;
+
                         c = clrIO[ci].inf(m);
                         c[0] = 255 - c[0];
                         c[1] = 255 - c[1];
                         c[2] = 255 - c[2];
+                        // TODO: we can keep the original/modified style in another style-sheet and
+                        // TODO: replace them via class property of element.
+                        // TODO: we also need to remove original style string in element
                         curElm.style[styleProp2check[ip]] = clrIO[ci].outf(c);
 
-                        bMatch = true;
-                        console.log('match');
                         break;
                     }
                 }
 
-                if (!bMatch) {
+                if (object.keys(sChanged).length == 0) {
                     console.log("not match: " + ss);
+                } else {
+                    // TODO: cache the modified value
                 }
             }
         }
     }
 
     for (i in curElm.childNodes) {
+        // we are only interested in nodeType==1, which means Element
         if (curElm.childNodes[i].nodeType == 1) {
             qElm.push(curElm.childNodes[i]);
         }
     }
 }
+
+// TODO: modify stylesheet
 
 })();
