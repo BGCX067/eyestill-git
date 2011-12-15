f962139dad7fc7870b70f0ffb48a6f52e58f54bc
missionliao <missionliao@missionliao-OptiPlex-760.(none)>
2011-12-15 13:29:23 +0800

    daily dev

---
+++ ext/eye_saver/cs.js
@@ -0,0 +1,8 @@
+(function() {
+
+h = new com.zuki.common.shortcutHandler();
+h.addHandler("up", {special_key:"ctrl", kc:"m"}, function() {
+    console.log('mission press alt m');
+});
+
+})();
--- src/com/zuki/common/shortcutHandler.js
+++ src/com/zuki/common/shortcutHandler.js
@@ -24,24 +24,36 @@ if (!com.zuki.common.shortcutHandler) com.zuki.common.shortcutHandler = function
 }
 
 com.zuki.common.shortcutHandler.prototype.addHandler = function(et, matchObj, func) {
+    // check input type
+    if ("kc" in matchObj) {
+        if (typeof matchObj.kc == "string") {
+            matchObj.kc = matchObj.kc.toLowerCase();
+        } else if (typeof matchObj.kc != "number") {
+            throw "com.zuki.common.shortcutHandler.prototype.addHandler: unknown match-object, type of m is wrong.";
+        }
+    }
+    if (typeof func != "function") {
+        throw "com.zuki.common.shortcutHandler.prototype.addHandler: func is not a function.";
+    }
+
     if (et in this._fnMap) { 
         this._fnMap[et] = this._fnMap[et].concat({m: matchObj, f:func});
         switch(et) {
             case "press":
                 if (this._bPressAttached == false) {
-                    document.addEventListener("keypress", this.onkeypress);
+                    document.addEventListener("keypress", this._onkeypress.bind(this));
                     this._bPressAttached = true;
                 }
                 break;
             case "down":
                 if (this._bDownAttached == false) {
-                    document.addEventListener("keydown", this.onkeydown);
+                    document.addEventListener("keydown", this._onkeydown.bind(this));
                     this._bDownAttached = true;
                 }
                 breakl
             case "up":
                 if (this._bUpAttached == false) {
-                    document.addEventListener("keyup", this.onkeyup);
+                    document.addEventListener("keyup", this._onkeyup.bind(this));
                     this._bUpAttached = true;
                 }
                 break;
@@ -58,23 +70,21 @@ com.zuki.common.shortcutHandler.prototype._isMatched = function(m, e) {
     if ("special_key" in m) {
         t = m.special_key.split(";");
         for (var i in t) {
-            if (t[i].length > 1) {
-                switch (t[i]) {
-                    case "shift":
-                        bMatched = (e.shiftKey == false);
-                        break;
-                    case "ctrl":
-                        bMatched = (e.ctrlKey == false);
-                        break;
-                    case "alt":
-                        bMatched = (e.altKey == false);
-                        break;
-                    case "meta":
-                        bMatched = (e.metaKey == false);
-                        break;
-                    default:
-                        throw "com.zuki.common.shortcutHandler._isMatched: Unknown special key";
-                }
+            switch (t[i]) {
+                case "shift":
+                    bMatched = (e.shiftKey == true);
+                    break;
+                case "ctrl":
+                    bMatched = (e.ctrlKey == true);
+                    break;
+                case "alt":
+                    bMatched = (e.altKey == true);
+                    break;
+                case "meta":
+                    bMatched = (e.metaKey == true);
+                    break;
+                default:
+                    throw "com.zuki.common.shortcutHandler._isMatched: Unknown special key";
             }
         }
     }
@@ -85,7 +95,7 @@ com.zuki.common.shortcutHandler.prototype._isMatched = function(m, e) {
             bMatched = (e.keycode == m.kc);
         } else if (typeof m.kc == "string") {
             if (m.kc.length > 1) throw "com.zuki.common.shortcutHandler._isMatched: keycode's length shouldn't be longer than 1."
-            bMatched = (m.kc == String.fromCharCode(e.keycode));
+            bMatched = (m.kc == String.fromCharCode(e.keyCode).toLowerCase());
         } else {
             throw "com.zuki.common.shortcutHandler._isMatched: keycode should contain number or char.";
         }
@@ -96,18 +106,48 @@ com.zuki.common.shortcutHandler.prototype._isMatched = function(m, e) {
     return bMatched;
 }   
 
-com.zuki.common.shortcutHandler.prototype.onkeypress = function(e) {
+com.zuki.common.shortcutHandler.prototype._onkeypress = function(e) {
+    console.log('press [' + e.keyCode + '][' + e.charCode + '][' + String.fromCharCode(e.keyCode) + '][' + String.fromCharCode(e.charCode) + ']');
+    var bHandled = false;
     for (i in this._fnMap.press) {
+        if (this._isMatched(this._fnMap.press[i].m, e) == true) {
+            bHandled = this._fnMap.press[i].f();
+            if (bHandled == true) {
+                break;
+            }
+        }
     }
+
+    return bHandled;
 }
 
-com.zuki.common.shortcutHandler.prototype.onkeydown = function(e) {
+com.zuki.common.shortcutHandler.prototype._onkeydown = function(e) {
+    console.log('down [' + e.keyCode + '][' + e.charCode + '][' + String.fromCharCode(e.keyCode) + '][' + String.fromCharCode(e.charCode) + ']');
+    var bHandled = false;
     for (i in this._fnMap.down) {
+        if (this._isMatched(this._fnMap.down[i].m, e) == true) {
+            bHandled = this._fnMap.down[i].f();
+            if (bHandled == true) {
+                break;
+            }
+        }
     }
+
+    return bHandled;
 }
 
-com.zuki.common.shortcutHandler.prototype.onkeyup = function(e) {
+com.zuki.common.shortcutHandler.prototype._onkeyup = function(e) {
+    console.log('up [' + e.keyCode + '][' + e.charCode + '][' + String.fromCharCode(e.keyCode) + '][' + String.fromCharCode(e.charCode) + ']');
+    var bHandled = false;
     for (i in this._fnMap.up) {
+        if (this._isMatched(this._fnMap.up[i].m, e) == true) {
+            bHandled = this._fnMap.up[i].f();
+            if (bHandled == true) {
+                break;
+            }
+        }
     }
+
+    return bHandled;
 }
 
